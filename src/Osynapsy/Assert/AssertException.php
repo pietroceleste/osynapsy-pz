a12fc689217b85d7799eccb1a18496072880e10b
pietr <pietr@NUCi5>
2020-05-19 08:34:36 +0200

    Aggiunta classe per la gestione delle asserzioni

---
+++ src/Osynapsy/Assert/AssertException.php
@@ -0,0 +1,4 @@
+<?php
+namespace Osynapsy\Assert;
+
+class AssertException extends \LogicException {}
---
+++ src/Osynapsy/Assert/Assertion.php
@@ -0,0 +1,90 @@
+<?php
+namespace Osynapsy\Assert;
+
+class Assertion
+{       
+    public static function between($value, $lowerLimit, $upperLimit, $message)
+    {
+        if ($lowerLimit > $value || $value > $upperLimit) {
+            self::raiseException($message);
+        }
+        return true;
+    }
+	
+	public static function digit($value, $message)
+	{
+		if (!\ctype_digit($value)){
+			self::raiseException($message);
+		}
+		return true;
+	}
+	
+    public static function equal($value1, $value2, $message)
+    {
+        if ($value1 != $value2) {
+            self::raiseException($message);
+        }
+        return true;
+    }
+    
+	public static function integer($value, $message)
+    {
+        if (!is_int($value)) {
+            self::raiseException($message);            
+        }
+        return true;
+    }
+	
+	public static function isEmpty($value, $message)
+    {
+        if (!empty($value)) {
+            self::raiseException($message);
+        }
+        return true;
+    }
+	
+	public static function greaterThan($value, $limit, $message)
+    {
+        if ($value <= $limit) {
+            self::raiseException($message);
+        }
+        return true;
+    }
+    
+    public static function greaterOrEqualThan($value, $limit, $message)
+    {
+        if ($value < $limit) {
+            self::raiseException($message);
+        }
+        return true;
+    }
+	
+    public static function lessThan($value, $limit, $message)
+    {
+        if ($value >= $limit) {
+            self::raiseException($message);
+        }
+        return true;
+    }
+    
+    public static function lessOrEqualThan($value, $limit, $message)
+    {
+        if ($value > $limit) {
+            self::raiseException($message);
+        }
+        return true;
+    }       
+    
+	public static function notEmpty($value, $message)
+    {
+        if (empty($value)) {
+            self::raiseException($message);
+        }
+        return true;
+    }
+	
+    protected static function raiseException($message, $code = null)
+    {
+        throw new AssertException($message, $code);
+    }
+}

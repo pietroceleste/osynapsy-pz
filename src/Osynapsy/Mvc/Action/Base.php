529c85eb4a9ea0eeda2cf8e08bd3652409e7b722
pietr <pietr@NUCi5>
2020-10-16 14:46:31 +0200

    Aggiunta nuovo package action

---
+++ src/Osynapsy/Mvc/Action/Base.php
@@ -0,0 +1,64 @@
+<?php
+namespace Osynapsy\Mvc\Action;
+
+use Osynapsy\Mvc\Controller;
+use Osynapsy\Db\Driver\InterfaceDbo;
+
+/**
+ * Description of Base
+ *
+ * @author Pietro
+ */
+abstract class Base implements InterfaceAction
+{
+    protected $controller;
+    protected $parameters;
+    protected $triggers = [];
+    
+    abstract public function execute();
+
+    protected function executeTrigger($eventId)
+    {
+        if (empty($this->triggers[$eventId])) {
+            return;
+        }
+        call_user_func($this->triggers[$eventId], $this);
+    }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    public function getDb()
+    {
+        return $this->controller->getDb();
+    }
+
+    public function getModel()
+    {
+        return $this->getController()->getModel();
+    }
+
+    public function getResponse()
+    {
+        return $this->getController()->getResponse();
+    }
+
+    public function setController(Controller $controller)
+    {
+        $this->controller = $controller;
+    }
+
+    public function setParameters(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    public function setTrigger(array $events, callable $function)
+    {
+        foreach ($events as $event) {
+            $this->triggers[$event] = $function;
+        }
+    }
+}

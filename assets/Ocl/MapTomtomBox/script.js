dd506fe81a6816ab9810e5b351c8da3a70ca20b8
pietr <pietr@NUCi5>
2021-03-02 18:05:52 +0100

    Minirefactoring della classe OClTomtomMap

--- assets/Ocl/MapTomtomBox/script.js
+++ assets/Ocl/MapTomtomBox/script.js
@@ -15,7 +15,7 @@ OclMapTomtomBox = {
         var self = this;
         $('.osy-mapgrid-tomtom').each(function(){
             var mapId = $(this).attr('id');
-            var zoom = 10;            
+            var zoom = 10;
             tomtom.setProductInfo('OclTomtomBox', '0.1');
             var map = tomtom.map(mapId, {
                 key: 'dXD9YwZw0BUAgA3VKh8YsOQKDEHcMbEO',
@@ -23,22 +23,21 @@ OclMapTomtomBox = {
                 basePath: '/sdk'
             });
             map.mapId = mapId;
-            self.maplist[mapId] = map;            
+            self.maplist[mapId] = map;
             if (document.getElementById(mapId + '_zoom').value > 0){
-                zoom = document.getElementById(mapId + '_zoom').value;			
+                zoom = document.getElementById(mapId + '_zoom').value;
             }
-            if (!Osynapsy.isEmpty( $('#' + mapId + '_center').val())) {                
-                var center = $('#' + mapId + '_center').val().split(',');                        
+            if (!Osynapsy.isEmpty( $('#' + mapId + '_center').val())) {
+                var center = $('#' + mapId + '_center').val().split(',');
                 center[0] = parseFloat(center[0]);
                 center[1] = parseFloat(center[1]);
                 map.setView(center, zoom);
-            }                        
+            }
             $('div[data-mapgrid=' + $(this).attr('id') +']').each(function(){
                 self.datagrid.push($(this).attr('id'));
             });
             self.setVertex(map);
             map.on('moveend', function(e) {
-		console.log(self.inupdate);
                 if (self.inupdate) {
                     return;
 		}
@@ -47,27 +46,23 @@ OclMapTomtomBox = {
                 self.setVertex(map);
 		if (OclMapTomtomBox.refreshGridTimeout) {
                     clearTimeout(OclMapTomtomBox.refreshGridTimeout);
-		}                
+		}
                 OclMapTomtomBox.refreshGridTimeout = setTimeout(function(){
-                    OclMapTomtomBox.refreshDatagrid(false);                    
+                    OclMapTomtomBox.refreshDatagrid(false);
                 }, 3000);
-            });                                                                                                                                       						                       
+            });
             if (Osynapsy.isEmpty($(this).data('marker'))){
                 return true;
             }
-            self.startMarker = $(this).data('marker').split(',');				
-            self.markersAdd(mapId, 'start-layer',[{
-                lat : parseFloat(self.startMarker[0]),
-                lng : parseFloat(self.startMarker[1]),
-                oid : mapId + '-start',
-                ico : {
-                    class : self.startMarker[2],
-                    color : 'red'
-                },
-                popup : 'MAIN'
-            }]); 
-        });		
-		this.refreshDatagrid(true);
+            self.startMarker = $(this).data('marker').split(',');
+            self.setStartMarker(
+                mapId,
+                self.startMarker[0],
+                self.startMarker[1],
+                self.startMarker[2]
+            );
+        });
+	this.refreshDatagrid(true);
     },
     calc_dist : function(sta, end)
     {
@@ -81,11 +76,11 @@ OclMapTomtomBox = {
 	//Alert impostando una distanza troppo bassa va in errore;
   	var dst_min = parseFloat(100000000);
 	var coo_min = null;
-	for (i in dat) {		     
+	for (i in dat) {
             var dst_cur = this.calc_dist(sta, dat[i]);
             dst_min = Math.min(dst_min,dst_cur);
-            if (dst_min == dst_cur){ 
-		coo_min = dat[i]; 
+            if (dst_min == dst_cur){
+		coo_min = dat[i];
             }
 	}
 	return coo_min;
@@ -103,10 +98,10 @@ OclMapTomtomBox = {
             try{
                 arr.push([parseFloat(nxt.lat),parseFloat(nxt.lng)]);
                 dat.splice( dat.indexOf(nxt),1);
-            } catch (err){         
+            } catch (err){
                 i = 100;
-            }		
-	}	  
+            }
+	}
 	if (mapid in this.maplist){
 	    if (polid in this.polylinelist){
                 this.maplist[mapid].removeLayer(this.polylinelist[polid]);
@@ -114,8 +109,8 @@ OclMapTomtomBox = {
             this.polylinelist[polid] = new L.polyline(arr,{color : 'red'});
             this.polylinelist[polid].addTo(this.maplist[mapid]);
             //this.layerlist[map].addLayer(pol);
-	}      
-    },   
+	}
+    },
     dataset_add : function(datid,dats)
     {
    	this.datasets[datid] = dats;
@@ -123,8 +118,8 @@ OclMapTomtomBox = {
     dataset_calc_route : function(mapid, datid, sta)
     {
         if (datid in this.datasets) {
-            var data = this.datasets[datid].slice();			
-            if (sta){ 
+            var data = this.datasets[datid].slice();
+            if (sta){
                 data.unshift(sta);
             }
             this.calc_perc(mapid,data);
@@ -140,7 +135,7 @@ OclMapTomtomBox = {
             this.cleanLayer(layerId);
         }
         return this.layerlist[layerId];
-    },   
+    },
     cleanLayer : function(layerId)
     {
         if (layerId in this.layerlist){
@@ -159,11 +154,11 @@ OclMapTomtomBox = {
         }
     },
     showLayer : function(layerId)
-    {        
+    {
         if (layerId in this.layerlist) {
             let layer = this.layerlist[layerId];
             this.maplist[layer.mapId].removeLayer(layer);
-            this.maplist[layer.mapId].addLayer(layer);            
+            this.maplist[layer.mapId].addLayer(layer);
         }
     },
     displayInstructions : function (routeJson, resultsContainer)
@@ -192,7 +187,7 @@ OclMapTomtomBox = {
     },
     getMarkerId : function(lat, lng, txt)
     {
-        var string = lat + ',' + lng + ','+ txt;        
+        var string = lat + ',' + lng + ','+ txt;
         return Osynapsy.hashCode(string);
     },
     markersClean : function(mapid)
@@ -200,25 +195,25 @@ OclMapTomtomBox = {
     },
     markersAdd : function(mapId, layerId, markers)
     {
-        if (!(markers instanceof Array)){ 
-            return; 
-        }        
+        if (!(markers instanceof Array)){
+            return;
+        }
         for (var i in markers){
-            var marker = markers[i];           
-            if (Osynapsy.isEmpty(marker.ico)) {                
+            var marker = markers[i];
+            if (Osynapsy.isEmpty(marker.ico)) {
                 continue;
-            }            
+            }
             if (!Osynapsy.isEmpty(marker.ico.class) && marker.ico.class.indexOf('fa-') === 0){
                 var ico = L.AwesomeMarkers.icon({
                     icon : marker.ico.class,
-                    prefix : 'fa', 
-                    markerColor : marker.ico.color, 
+                    prefix : 'fa',
+                    markerColor : marker.ico.color,
                     spin : false
-                });  
+                });
             } else {
                 var ico = L.divIcon({
-                    className: 'osy-mapgrid-marker ' + (Osynapsy.isEmpty(marker.ico.class) ? 'osy-mapgrid-marker-blue' : marker.ico.class), 
-                    html : marker.ico.text, 
+                    className: 'osy-mapgrid-marker ' + (Osynapsy.isEmpty(marker.ico.class) ? 'osy-mapgrid-marker-blue' : marker.ico.class),
+                    html : marker.ico.text,
                     iconSize : null,
                     popupAnchor : [0, -35]
                 });
@@ -231,27 +226,27 @@ OclMapTomtomBox = {
                 markerObject.bindPopup(marker.popup);
             }
             this.markerAppend(mapId, layerId, marker['id'], markerObject);
-        }        
+        }
     },
     markerAppend : function(mapId, layerId, markerId, markerObject)
-    {        
+    {
         if (!(layerId in this.layermarker)){
             this.layermarker[layerId] = {};
-        }        
-        this.layermarker[layerId][markerId] = markerObject; 
+        }
+        this.layermarker[layerId][markerId] = markerObject;
         this.getLayer(mapId, layerId).addLayer(markerObject);
     },
     polyline : function(mapId, layerId, dataset, polylineColor)
     {
         if (polylineColor === undefined || polylineColor === null) {
             polylineColor = 'red';
-        }        
+        }
         if (mapId in this.maplist) {
             var layer = this.getLayer(mapId, layerId, false);
             var polyline = new L.polyline(dataset, {color : polylineColor});
-            polyline.addTo(layer);	  	
-        } 
-    },   
+            polyline.addTo(layer);
+        }
+    },
     refreshDatagrid : function(initialize)
     {
         if (this.datagrid.length === 0) {
@@ -268,12 +263,12 @@ OclMapTomtomBox = {
         this.cleanLayer(layerId);
         if (Osynapsy.isEmpty(dataset)) {
            return;
-        }       
+        }
         var markers = [];
-        for (var i in dataset) {            
+        for (var i in dataset) {
             var rawMarker = Array.isArray(dataset[i]) ? dataset[i] : dataset[i].split(',');
-            var infoWindow = rawMarker.length === 7 ? rawMarker[6] : '';  
-            infoWindow = '<div class="infoWindow" style="width: 250px;">'+ infoWindow +'</div>';            
+            var infoWindow = rawMarker.length === 7 ? rawMarker[6] : '';
+            infoWindow = '<div class="infoWindow" style="width: 250px;">'+ infoWindow +'</div>';
             var marker = {
                 id  : rawMarker[0],
                 lat : Osynapsy.isEmpty(rawMarker[1]) ? null : parseFloat(rawMarker[1]),
@@ -282,14 +277,14 @@ OclMapTomtomBox = {
                     class : Osynapsy.isEmpty(rawMarker[3]) ? 'fa-circle' : rawMarker[3],
                     text  : Osynapsy.isEmpty(rawMarker[4]) ? '' : rawMarker[4],
                     color : Osynapsy.isEmpty(rawMarker[5]) ? 'blue' : rawMarker[5]
-                },                
-                popup : infoWindow                
-            };                       
-            if (!Osynapsy.isEmpty(marker.lat) && !Osynapsy.isEmpty(marker.lng)){               
+                },
+                popup : infoWindow
+            };
+            if (!Osynapsy.isEmpty(marker.lat) && !Osynapsy.isEmpty(marker.lng)){
                 markers.push(marker);
-            }            
-        }        
-        this.markersAdd(mapId, layerId, markers);                        
+            }
+        }
+        this.markersAdd(mapId, layerId, markers);
         if (colorRoute) {
             this.routing(mapId, markers, layerId + '_route', colorRoute);
         } else if (autoCenter) {
@@ -297,10 +292,10 @@ OclMapTomtomBox = {
         }
     },
     refreshMarkers : function(mapId, dataGridId)
-    {        
-	if (this.datagrid.length === 0) { 
-            return; 
-	}        
+    {
+	if (this.datagrid.length === 0) {
+            return;
+	}
 	var dataGrid = $('#'+dataGridId);
         var infoFormat = dataGrid.data('mapgrid-infowindow');
 	var dataset = [];
@@ -314,13 +309,13 @@ OclMapTomtomBox = {
             $(this).children().each(function(){
                if (Osynapsy.isEmpty(infoFormat)){
                    infoWindow += $(this).text() + '<br>';
-                } else if (infoWindow.indexOf('['+i+']') > -1) { 
+                } else if (infoWindow.indexOf('['+i+']') > -1) {
                    infoWindow = infoWindow.replace('['+i+']',$(this).html());
                 }
                 i++;
             });
             infoWindow = '<div class="infoWindow" style="width: 250px;">'+ infoWindow +'</div>';
-            var rawMarker = $(this).data('marker').split(',');           
+            var rawMarker = $(this).data('marker').split(',');
             var marker = {
                 id  : rawMarker[0],
                 lat : Osynapsy.isEmpty(rawMarker[0]) ? null : parseFloat(rawMarker[1]),
@@ -329,13 +324,13 @@ OclMapTomtomBox = {
                     class : Osynapsy.isEmpty(rawMarker[3]) ? 'fa-circle' : rawMarker[3],
                     text  : Osynapsy.isEmpty(rawMarker[4]) ? '' : rawMarker[4],
                     color : Osynapsy.isEmpty(rawMarker[5]) ? 'blue' : rawMarker[5]
-                },                
+                },
                 popup : infoWindow
-            };            
-            if (!Osynapsy.isEmpty(marker.lat) && !Osynapsy.isEmpty(marker.lng)){               
+            };
+            if (!Osynapsy.isEmpty(marker.lat) && !Osynapsy.isEmpty(marker.lng)){
                 dataset.push(marker);
-            }            
-        });        
+            }
+        });
         this.markersAdd(mapId, dataGridId, dataset);
         if (this.autocenter) {
            this.computeCenter(mapId, dataset);
@@ -350,26 +345,26 @@ OclMapTomtomBox = {
     routing : function(mapId, dataset, layerId, colorLayer)
     {
         //Init routing layer
-        if (Osynapsy.isEmpty(layerId)) { 
+        if (Osynapsy.isEmpty(layerId)) {
             layerId = 'routing';
         }
-        if (Osynapsy.isEmpty(colorLayer)) { 
+        if (Osynapsy.isEmpty(colorLayer)) {
             colorLayer = '#00d7ff';
-        }        
+        }
         var map = this.maplist[mapId];
         var coordinates = [];
-        if (!(layerId in this.layerlist)) {                   
+        if (!(layerId in this.layerlist)) {
             this.layerlist[layerId] = tomtom.L.geoJson(null,{
                 style: {
                     color: colorLayer,
-                    opacity: 0.8, 
+                    opacity: 0.8,
                     zIndexOffset : 100
                 }
-            }).addTo(map);            
-        } else {            
+            }).addTo(map);
+        } else {
             this.layerlist[layerId].clearLayers();
-        }        
-        if (!Osynapsy.isEmpty(this.startMarker)) {         
+        }
+        if (!Osynapsy.isEmpty(this.startMarker)) {
             coordinates.push(this.startMarker[0] + ',' + this.startMarker[1]);
         }
         if (Osynapsy.isEmpty(dataset) || (dataset.length + coordinates.length) < 2) {
@@ -378,14 +373,14 @@ OclMapTomtomBox = {
         }
         for (var i in dataset) {
             coordinates.push(dataset[i].lat + ',' + dataset[i].lng);
-        }        
+        }
         var self = this;
         tomtom.routing()
               .locations(coordinates.join(':'))
               .go()
-              .then(function(routeJson) {                    
+              .then(function(routeJson) {
                     var layer = self.layerlist[layerId];
-                    layer.addData(routeJson);                                        
+                    layer.addData(routeJson);
                });
     },
     computeCenter : function(mapId, dataset)
@@ -400,34 +395,34 @@ OclMapTomtomBox = {
             center.lng += rec['lng'];
         }
         center.lat = center.lat / (parseInt(i) + 1);
-        center.lng = center.lng / (parseInt(i) + 1);        
+        center.lng = center.lng / (parseInt(i) + 1);
         this.setCenter(mapId, center);
     },
     setVertex : function(map){
 		var mapId = map.mapId;
 		var bounds = map.getBounds();
-			
+
 		var ne = bounds.getNorthEast();
 		var sw = bounds.getSouthWest();
 		var center = map.getCenter();
 		//console.log(ne,sw,center.toString(), map.getContainer().getAttribute('id'));
-			
+
 		$('#'+mapId+'_ne_lat').val(ne.lat);
 		$('#'+mapId+'_ne_lng').val(ne.lng);
 		$('#'+mapId+'_sw_lat').val(sw.lat);
-		$('#'+mapId+'_sw_lng').val(sw.lng); 
+		$('#'+mapId+'_sw_lng').val(sw.lng);
 			//return;
-		//$('#'+mapId+'_center').val(map.getCenter().toString().replace('LatLng(','').replace(')','')); 
-		$('#'+mapId+'_cnt_lat').val((sw.lat + ne.lat) / 2); 
-		$('#'+mapId+'_cnt_lng').val((sw.lng + ne.lng) / 2); 
-    },	  
+		//$('#'+mapId+'_center').val(map.getCenter().toString().replace('LatLng(','').replace(')',''));
+		$('#'+mapId+'_cnt_lat').val((sw.lat + ne.lat) / 2);
+		$('#'+mapId+'_cnt_lng').val((sw.lng + ne.lng) / 2);
+    },
     openId : function(markerId, layerId)
-    {   	 		
+    {
 		if (Osynapsy.isEmpty(layerId)){
 				this.markerlist[markerId].openPopup();
 				return;
-		}        
-        if ((layerId in this.layermarker) && (markerId in this.layermarker[layerId])){            
+		}
+        if ((layerId in this.layermarker) && (markerId in this.layermarker[layerId])){
             this.layermarker[layerId][markerId].openPopup();
         }
     },
@@ -437,13 +432,23 @@ OclMapTomtomBox = {
             this.maplist[mapid].invalidateSize();
 	}
     },
-    setCenter: function(mid,cnt,zom)
+    setCenter: function(mapId, position ,zoomLevel)
     {
-		this.maplist[mid].setView(cnt,zom);
+	this.maplist[mapId].setView(position, zoomLevel);
+    },
+    setStartMarker : function(mapId, latitude, longitude, iconClass)
+    {
+        this.markersAdd(mapId, 'start-layer',[{
+            lat : parseFloat(latitude),
+            lng : parseFloat(longitude),
+            oid : mapId + '-start',
+            ico : {class : iconClass, color : 'red'},
+            popup : 'MAIN'
+        }]);
     }
 }
 
-if (window.FormController){    
+if (window.FormController){
     FormController.register('init','OclMapTomtomBox',function(){
         OclMapTomtomBox.init();
     });

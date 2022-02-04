2a7f1ca7d39deefd0c0a7330388c54af179fd55a
pietr <pietr@NUCi5>
2022-02-04 15:26:41 +0100

    Refactoring del build component

--- src/Osynapsy/Html/Bcl/BoxGrid.php
+++ src/Osynapsy/Html/Bcl/BoxGrid.php
@@ -5,85 +5,100 @@ use Osynapsy\Html\Component;
 use Osynapsy\Html\Tag;
 
 class BoxGrid extends Component
-{    
+{
     private $boxWidth = 4;
-    private $emptyMessage = 'No data found';    
-    private $addComand = false;
-    private $panel;    
-    
+    private $emptyMessage = '';
+    private $addComand;
+    private $panel;
+
     public function __construct($id)
     {
-        parent::__construct('div', $id);        
+        parent::__construct('div', $id);
         $this->panel = $this->add(new PanelNew($id.'Panel'));
         $this->panel->resetClass();
-        $this->setClass('osy-gridbox');        
+        $this->setClass('osy-gridbox');
     }
-    
+
     protected function __build_extra__()
-	{                
+	{
          if (empty($this->data)) {
-            $this->appendCell($this->emptyMessage, 12)->setClass('text-center');
+            if (!empty($this->emptyMessage)) {
+                $this->appendCell($this->emptyMessage, 12)->setClass('text-center');
+            }
             return;
         }
-        $i = 12;
-        if ($this->addComand) {
-            $i -= $this->boxWidth;
-        }
-        foreach($this->data as $value) {            
-            if (empty($i)) {
-                $this->getPanel()->addRow()->att('style','margin-top: 10px !important;');
-                $i = 12;
+        $i = 0;
+        foreach($this->data as $value) {
+            if ($i >= 12) {
+                $this->addRowToPanel();
+                $i = 0;
             }
             $this->appendCell($value, $this->boxWidth);
-            $i -= $this->boxWidth;
-        }    
-    }        
-        
+            $i += $this->boxWidth;
+        }
+        if ($i >= 12) {
+            $this->addRowToPanel();
+        }
+        if ($this->addComand) {
+            $this->appendCell($this->addComand, $this->boxWidth, '1px dashed silver')->setClass('text-center');
+        }
+    }
+
+    protected function addRowToPanel()
+    {
+        $this->getPanel()->addRow()->att('style','margin-top: 10px !important;');
+    }
+
     public function appendCell($rawValue, $width, $borderStyle = '1px solid silver')
-    {        
-        $column = $this->getPanel()->addColumn($width);        
-        $cellCont = $column->add(new Tag('div', null, 'osy-gridbox-cell'))                           
-                           ->att('style','border: '.$borderStyle.'; border-radius: 5px; padding: 5px; 5px;');
+    {
+        $column = $this->getPanel()->addColumn($width);
+        $column->add($this->cellFactory($rawValue, $borderStyle));
+        return $column;
+    }
+
+    protected function cellFactory($rawValue, $borderStyle)
+    {
+        $cellCont = new Tag('div', null, 'osy-gridbox-cell');
+        $cellCont->att('style','border: '.$borderStyle.'; border-radius: 5px; padding: 5px; 5px;');
         $cellBody = $cellCont->add(new Tag('div', null, 'osy-gridbox-cell-body'));
         $cellFoot = $cellCont->add(new Tag('div', null, 'osy-gridbox-cell-foot clearfix'));
         if (!is_array($rawValue)) {
             $cellBody->add($rawValue);
             $cellFoot->add('&nbsp;');
-            return $column;
+            return $cellCont;
         }
         $commands = $value = $label = [];
         foreach($rawValue as $key => $val) {
             if ($key[0] !== '_') {
-                $value[] = $val;                    
+                $value[] = $val;
             } elseif ($key === '_cmd') {
-                $commands[] = $val;                
+                $commands[] = $val;
             } elseif (strpos($key,'_label') === 0 && !empty($val)) {
                 $labelcss = substr($key, 1);
                 $label[] = '<span class="label '.$labelcss.'">'.$val.'</span>';
-            }                
+            }
         }
         $cellBody->add(implode('<br>',$value));
         $cellFoot->add('<div class="pull-left">'.implode('&nbsp;',$label).'</div>');
         $cellFoot->add('<div class="pull-right">'.implode('&nbsp;',$commands).'</div>');
-        return $column;
-    }        
-    
+        return $cellCont;
+    }
+
     public function appendAddCommand($command)
     {
-        $this->appendCell($command, $this->boxWidth, '1px dashed silver')->setClass('text-center');
-        $this->addComand = true;
+        $this->addComand = $command;
     }
-    
+
     public function getPanel()
     {
         return $this->panel;
     }
-    
+
     public function setDefaultBoxWidth($width)
     {
         $this->boxWidth = $width;
     }
-    
+
     public function setEmptyMessage($message)
     {
         $this->emptyMessage = $message;

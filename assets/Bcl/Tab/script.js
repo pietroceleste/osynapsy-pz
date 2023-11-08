c110ec9adcc1f55add4cb58af9426dfbb99bc6f8
Pietro Celeste <p.celeste@osynapsy.net>
2023-11-08 15:31:58 +0100

    Refactoring Tab component

--- assets/Bcl/Tab/script.js
+++ assets/Bcl/Tab/script.js
@@ -1,25 +1,20 @@
 BclTab = {
     init : function()
-    {
-        $('.nav-tabs').each(function(){
-           
-           var tabSelected = $('input[type=hidden]',$(this).closest('div')).val();
-           if (tabSelected) {
-               $('a[href="'+tabSelected+'"]',this).tab('show')
-           } else {
-               $('a:first',this).tab('show');
-           }
-           $('a',this).click(function(){
-               var tabid = $(this).attr('href');
-               var hdnid = $(this).closest('ul').attr('id').replace('_nav','');
-               $('#'+hdnid).val(tabid);
-               //$('input[type=hidden]',$(this).closest('div')).val(tabid);
+    {        
+        $('.nav-tabs').each(function() {            
+           let tabSelected = $('input[type=hidden]', this).val();           
+           let aSelected = (tabSelected !== '') ? 'a[href="'+tabSelected+'"]' : 'a:first';           
+           $(aSelected, this).tab('show');           
+           $('a', this).click(function() {
+               let tabId = $(this).attr('href');
+               let hdnId = $(this).closest('ul').attr('id').replace('_nav','');
+               $('#'+hdnId).val(tabId);
            });
         });
     }
-}
+};
 
-if (window.FormController){    
+if (window.FormController){
     FormController.register('init','BclTab_Init',function(){
         BclTab.init();
     });

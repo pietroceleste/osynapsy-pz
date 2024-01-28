2a4ff14ed0cbdb3db72d2117671505a0b4eb2170
Pietro Celeste <p.celeste@osynapsy.net>
2024-01-28 08:04:16 +0100

    fix

--- src/Osynapsy/Http/RequestRaw.php
+++ src/Osynapsy/Http/RequestRaw.php
@@ -1,9 +1,12 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Osynapsy\Http;
@@ -20,32 +23,32 @@ class RequestRaw
     
     public function __construct()
     {
-		$this->server = $_SERVER;
+	$this->server = $_SERVER;
         $data = sprintf(
-			"%s %s %s\n\nHTTP headers:\n",
-			$this->server['REQUEST_METHOD'],
-			$this->server['REQUEST_URI'],
-			$this->server['SERVER_PROTOCOL']
-		);
-		foreach ($this->getHeaderList() as $name => $value) {
-			$data .= $name . ': ' . $value . "\n";
-		}
-		$data .= "\nRequest body:\n";
-		
-		$this->raw = $data;
-	}
+            "%s %s %s\n\nHTTP headers:\n",
+            $this->server['REQUEST_METHOD'],
+            $this->server['REQUEST_URI'],
+            $this->server['SERVER_PROTOCOL']
+        );
+        foreach ($this->getHeaderList() as $name => $value) {
+                $data .= $name . ': ' . $value . "\n";
+        }
+        $data .= "\nRequest body:\n";
+
+        $this->raw = $data;
+    }
 	
     private function getHeaderList()
     {
-		$headerList = [];
-		foreach ($this->server as $key => $value) {
-			if (preg_match('/^HTTP_/',$key)) {								
-				// add to list
-				$headerList[$this->convertHeaderKey($key)] = $value;
-			}
-		}
-		return $headerList;
-	}
+        $headerList = [];
+        foreach ($this->server as $key => $value) {
+            if (preg_match('/^HTTP_/',$key)) {								
+                // add to list
+                $headerList[$this->convertHeaderKey($key)] = $value;
+            }
+        }
+        return $headerList;
+    }
     
     /**
      * convert HTTP_HEADER_NAME to Header-Name
@@ -56,8 +59,8 @@ class RequestRaw
     private function convertHeaderKey($key)
     {
         $httpHeaderKey = strtr(substr($key,5),'_',' ');
-		$httpHeaderKey = ucwords(strtolower($httpHeaderKey));
-		return strtr($httpHeaderKey,' ','-');        
+        $httpHeaderKey = ucwords(strtolower($httpHeaderKey));
+        return strtr($httpHeaderKey,' ','-');
     }
     
     public function get()

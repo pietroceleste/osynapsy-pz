ac0f825dd1b927c09f3a7314f1a5fb8e031a1b9c
Pietro Celeste <p.celeste@osynapsy.net>
2024-02-27 08:52:19 +0100

    .

--- src/Osynapsy/Event/InterfaceListener.php
+++ src/Osynapsy/Event/InterfaceListener.php
@@ -16,11 +16,11 @@ use Osynapsy\Mvc\Controller;
 /**
  * Description of Listener
  *
- * @author pietr
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
-interface InterfaceListener 
+interface InterfaceListener
 {
     public function __construct(Controller $controller);
-    
+
     public function trigger();
 }

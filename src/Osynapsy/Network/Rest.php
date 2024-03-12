a8f2fd418d6860a60c3f7d2110c20ae3a2ec297d
Pietro Celeste <p.celeste@osynapsy.net>
2024-03-12 08:57:06 +0100

    Aggiornato client Rest

--- src/Osynapsy/Network/Rest.php
+++ src/Osynapsy/Network/Rest.php
@@ -13,22 +13,25 @@ namespace Osynapsy\Network;
 
 class Rest
 {
-    public static function get($url)
+    public static function get($endpoint, $data = [])
     {
+        $url = $endpoint;
+        if (!empty($data)) {
+           $url .= '?'.http_build_query($data);
+        }
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        //curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
-       return curl_exec($ch);  
+	return curl_exec($ch);
     }
-    
-    public static function post($url, $data, $header = null)
+
+    public static function post($url, $data, array $headers = [])
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        //curl_setopt($ch, CURLOPT_HEADER,true);
-        if (!empty($header)) {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        if (!empty($headers)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
@@ -36,24 +39,26 @@ class Rest
         //curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-        $resp = curl_exec($ch);
-        
+        $response = curl_exec($ch);
+        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
         $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
         curl_close($ch);
-        
-        return array($contentType, $resp);
+        return [
+            'content-type' => $contentType,
+            'header' => substr($response, 0, $headerSize),
+            'body' => substr($response, $headerSize),
+            'raw' => $response
+        ];
     }
-    
-    public static function postJson($url, $data)
+
+    public static function postJson($url, $data, $headers = [])
     {
         $json = json_encode($data, JSON_HEX_TAG|JSON_HEX_APOS|JSON_HEX_QUOT|JSON_HEX_AMP);
-        
-        $header = array(
+        $response = self::post($url, $json, $headers + [
             'Content-Type: application/json',
             'Content-Length: ' . strlen($json)
-        );
-        
-        return self::post($url, $json, $header);
+        ]);
+        $response['body'] = json_decode($response['body'], true) ?? $response['body'];
+        return $response;
     }
 }

b5b679d9596724db5e90508eea24a92c0fbdc15c
Pietro Celeste <p.celeste@osynapsy.net>
2024-08-29 18:56:07 +0200

    Improve code

--- src/Osynapsy/Network/Rest.php
+++ src/Osynapsy/Network/Rest.php
@@ -18,11 +18,19 @@ class Rest
         $url = $endpoint;
         if (!empty($data)) {
            $url .= '?'.http_build_query($data);
-        }
-        $ch = curl_init($url);
+        }        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-	return curl_exec($ch);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$response = curl_exec($ch);
+        if ($response===false) {
+            throw new \Exception(curl_error($ch));
+        }
+        curl_close($ch);
+        return $response;
     }
 
     public static function post($url, $data, array $headers = [])
@@ -51,14 +59,21 @@ class Rest
         ];
     }
 
-    public static function postJson($url, $data, $headers = [])
+    public static function postJson($url, $data, array $rawheaders = [])
     {
         $json = json_encode($data, JSON_HEX_TAG|JSON_HEX_APOS|JSON_HEX_QUOT|JSON_HEX_AMP);
-        $response = self::post($url, $json, $headers + [
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($json)
-        ]);
+        $headers = self::array2header($rawheaders + ['Content-Type' => 'application/json', 'Content-Length' => strlen($json)]);        
+        $response = self::post($url, $json, $headers);
         $response['body'] = json_decode($response['body'], true) ?? $response['body'];
         return $response;
+    }
+    
+    private static function array2header(array $array = [])
+    {        
+        return array_map(
+            fn($key, $value) => sprintf('%s: %s', strtolower($key), $value), 
+            array_keys($array),
+            $array
+        );
     }
 }

904ae75327f102fd3ebc6eb3df7246995cb039a5
Pietro Celeste <p.celeste@osynapsy.net>
2025-08-07 08:50:02 +0200

    Bugfix - il nome della proietà content-type era errato e non veniva letto dal server openai

--- src/Osynapsy/AI/OpenAI/Client.php
+++ src/Osynapsy/AI/OpenAI/Client.php
@@ -7,70 +7,65 @@ use Osynapsy\AI\OpenAI\Message\Message;
 use Osynapsy\AI\OpenAI\Message\MessageInterface;
 
 class Client
-{    
+{
     const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
-    
+
     protected $version;
     protected $model;
     protected $key;
     protected $cache = False;
-    
+
     public function __construct(string $key, ?ModelInterface $model = null)
     {
-        $this->key = $key;        
+        $this->key = $key;
         $this->model = $model ?? new Model\Gpt_4o_mini();
     }
-    
+
     public function getModel() : ModelInterface
     {
         return $this->model;
     }
-    
+
     public function send(Message $prompt, $maxTokens = 1024)
-    {        
-        $headers = $this->headerRequestFactory();
-        $body = $this->bodyRequestFactory($prompt, $maxTokens);        
+    {
+        $headers = $this->headerRequestFactory($this->key);
+        $body = $this->bodyRequestFactory($prompt, $maxTokens);
         $response = Rest::postJson(self::API_ENDPOINT, $body, $headers);
         return $response['body'];
     }
-    
-    protected function headerRequestFactory()
+
+    protected function headerRequestFactory($key)
     {
-        $headers = [
-            'content-type' => 'application/json',            
-            'anthropic-version' => $this->version,
-            'Authorization' =>  sprintf('Bearer %s', $this->key)
-        ];        
-        if (!empty($this->cache)) {
-            $headers['anthropic-beta'] = 'prompt-caching-2024-07-31';
-        }
-        return $headers;
+        return [
+            'Content-Type' => 'application/json',
+            'Authorization' =>  sprintf('Bearer %s', $key)
+        ];
     }
-    
+
     protected function bodyRequestFactory(Message $prompt, $maxTokens) : array
-    {              
+    {
         $body = [
-            'model' => $this->getModel()->getId(),            
-            'messages' => $prompt->get()            
+            'model' => $this->getModel()->getId(),
+            'messages' => $prompt->get()
         ];
         if (!empty($maxTokens)) {
             $body['max_tokens'] = $maxTokens;
         }
         return $body;
     }
-    
+
     public function messageFactory() : MessageInterface
     {
-        return new Message;        
+        return new Message;
     }
-    
+
     protected function setVersion(string $ver) : void
     {
         $this->version = $ver;
     }
-    
+
     public function enableCache()
     {
-        $this->cache = True;
+        $this->cache = False;
     }
 }
